999fb542fcecb8a7d395e9cb55a15958c5f3d8e7
empflow <empflow@tutanota.com>
2023-11-14 20:03:14 +0300

    add hero images (desktop & mobile)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "@/app/ui/home.module.css";
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
-
 import Link from "next/link";
-import cn from "./lib/cn";
 import { lusitana } from "./ui/fonts";
+import Image from "next/image";
+import heroDesktop from "@/public/hero-desktop.png";
+import heroMobile from "@/public/hero-mobile.png";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -30,8 +32,14 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+        <div className="flex flex-col items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          <Image
+            alt="Hero image"
+            src={heroDesktop}
+            className="hidden md:block"
+          />
+          <Image alt="Hero image" src={heroMobile} className="md:hidden" />
+          hello
         </div>
       </div>
     </main>
