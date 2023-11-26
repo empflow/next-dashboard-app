360e4a390c1684cda9a3fc1d304dc6cc76390359
empflow <empflow@tutanota.com>
2023-11-26 11:48:12 +0300

    remove artificial load time increase

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,12 +4,9 @@ import { Suspense } from "react";
 import { Metadata } from "next";
 import DashboardCards from "../ui/Cards/Cards";
 import {
-  CardsSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from "../../ui/skeletons";
-import wait from "@/app/lib/wait";
-import DashboardCardsContent from "../ui/Cards/CardsContent";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -18,15 +15,10 @@ export const metadata: Metadata = {
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  await wait(2000);
   return (
     <main>
       <h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />}>
-          <DashboardCardsContent />
-        </Suspense>
-      </div>
+      <DashboardCards />
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
