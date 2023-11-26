8137c3d3a158f35106619dbf83ea5ca9778485cd
empflow <empflow@tutanota.com>
2023-11-26 11:37:18 +0300

    add artificial delay to /dashboard

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,10 +4,12 @@ import { Suspense } from "react";
 import { Metadata } from "next";
 import DashboardCards from "../ui/Cards/Cards";
 import {
+  CardsSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from "../../ui/skeletons";
 import wait from "@/app/lib/wait";
+import DashboardCardsContent from "../ui/Cards/CardsContent";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -16,10 +18,15 @@ export const metadata: Metadata = {
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
+  await wait(2000);
   return (
     <main>
       <h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
-      <DashboardCards />
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <DashboardCardsContent />
+        </Suspense>
+      </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
