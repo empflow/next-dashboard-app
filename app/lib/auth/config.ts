f69d2bbb1605294f07a87c907ecf1b034612687e
empflow <empflow@tutanota.com>
2023-12-06 09:18:12 +0300

    don't redirect away from / if logged in

--- app/lib/auth/config.ts
+++ app/lib/auth/config.ts
@@ -13,7 +13,7 @@ const authConfig = {
         if (isLoggedIn) return true;
         return false; // redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
+        return true;
       }
       return true;
     },
